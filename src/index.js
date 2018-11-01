e000b9fffbc06c2562f2a65f077b7eeb384436fe
Tatiana Karavaynaya <Tatiana.Karavaynaya@visioncritical.com>
2018-10-31 17:07:16 -0700

    Crated TODO app

--- src/index.js
+++ src/index.js
@@ -1,39 +1,141 @@
+'use strict'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const todoList = ['Item1', 'Item2'];
 
-function ToDoList(props) {        
-    return (
-        <div>           
-            {props.todoList.map((item, index) => <li key = {index}>{item}</li>)}            
-        </div>
-    );
-};
 
-function AddItem() {
-    function addNewItem() {
-        // todo
+class Item extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isDone: false,
+            itemStyle: {
+                "textDecoration": ""
+            }
+        };
+        this.handleClick = this.handleClick.bind(this);
     }
-    return(
-        <div>
-            <input type = "text" name = "newItem" />
-            <button type = "button" onClick = {() => addNewItem()}>Add new Item</button>
-        </div>
-    );
-};
 
-function ToDo() {
-    return(
-        <div>
-            <ToDoList todoList = {todoList} />
-            <br/><br/><br/>
-            <AddItem />
-        </div>
-    );
+    handleClick() {
+        if(!this.state.isDone) {
+            this.setState({
+                isDone: !this.state.isDone,
+                itemStyle: {"textDecoration": "line-through"}
+            });            
+        } else {
+            this.setState({
+                isDone: !this.state.isDone,
+                itemStyle: {"textDecoration": ""}
+            });
+        }
+    }
+
+    render() {
+        return (
+            <ul               
+                style = {this.state.itemStyle}
+                onClick = {this.handleClick}
+            >
+                {this.props.item}
+            </ul>
+        );
+    }
 }
 
+class NewItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleNewItemTextChange = this.handleNewItemTextChange.bind(this);
+        this.handleAddNewItemClick = this.handleAddNewItemClick.bind(this);
+    }
+
+    handleNewItemTextChange(e) {
+        this.props.onNewItemTextChange(e.target.value);
+    }
+
+    handleAddNewItemClick(e) {
+        return;
+    }
+
+    render() {
+        return(
+            <form>
+                <input 
+                    type = "text" 
+                    placeholder = "New Item..." 
+                    value = {this.props.newItemText}
+                    onChange = {this.handleNewItemTextChange}
+                />
+                <button 
+                    type = "submit"
+                    onClick = {this.handleAddNewItemClick}
+                >
+                    Add
+                </button>
+            </form>
+        );
+    }
+}
+
+class ToDoItemList extends React.Component {     
+    render() {
+        const items = this.props.items;   
+        const toDoList = [];
+        
+        for(let item of items) {
+            if(item){
+                toDoList.push(
+                    <Item 
+                        item = {item} 
+                        key = {items.indexOf(item)}                        
+                    />
+                );
+            }
+        }
+            return(
+            <div>
+                {toDoList}
+            </div>
+        );
+    }
+}
+
+class ToDoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newItemText: ''
+        }
+
+        this.handleNewItemTextChange = this.handleNewItemTextChange.bind(this);
+    }
+
+    handleNewItemTextChange(newItemText) {
+        this.setState({
+            newItemText: newItemText
+        });
+    }
+    
+    render() {
+        return(
+            <div>
+                <NewItem 
+                    newItemText = {this.state.newItemText}
+                    onNewItemTextChange = {this.handleNewItemTextChange} />
+                <ToDoItemList items = {this.props.items} />
+            </div>
+        );
+    }
+}
+
+const ITEMS = [
+    'Item1',
+    'Item2',
+    'Item3'
+];
+
 ReactDOM.render(
-    <ToDo />,
+    <ToDoList items = {ITEMS}/>,
     document.getElementById('root')
 );
